afac76c08f67cf0bdf3955c27eec4302032bf788
Morgan M <m.murungi0@students.ku.ac.ke>
2020-09-22 16:09:27 +0300

    fetch

--- register.js
+++ register.js
@@ -1,5 +1,7 @@
 import handler from './controllers/handler/handler-lib';
 import axios from 'axios';
+import fetch from 'node-fetch';
+
 export const main = handler(async (event, context) => {
     let url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
     /*eslint new-cap: [2, {"newIsCapExceptions": ["Buffer.from"]}]*/
@@ -25,21 +27,22 @@ function register(data){
 
     let regurl = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
     let regauth = "Bearer " + data;
-    return axios.post(regurl,{
-
-      headers: {
-          "Authorization": regauth
-      },
-      json: {
-          "ShortCode": "600730",
-          "ResponseType": "Complete",
-          "ConfirmationURL": "http://192.168.1.50:3000/confirmation",
-          "ValidationURL": "http://192.168.1.50:3000/validation"
-      }
-  }).then(function (response) {
-      // handle success
-      return response;
-
+    return fetch(regurl,
+        {
+            method: 'POST',
+            headers: {
+                "Authorization": regauth,
+                "Content-Type" : 'application/json'
+            },
+            body: JSON.stringify({
+                ShortCode: "600730",
+                ResponseType: "Complete",
+                ConfirmationURL: "http://192.168.1.50:3000/confirmation",
+                ValidationURL: "http://192.168.1.50:3000/validation"
+            }),
+        }
+    ).then(res => {
+        return res;
     })
     .catch(function (error) {
       // handle error
--- simulate.js
+++ simulate.js
@@ -1,5 +1,6 @@
 import handler from './controllers/handler/handler-lib';
 import axios from 'axios';
+import fetch from 'node-fetch';
 export const main = handler(async (event, context) => {
     let url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
     /*eslint new-cap: [2, {"newIsCapExceptions": ["Buffer.from"]}]*/
@@ -26,18 +27,19 @@ function simulate(data){
     let simurl = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate";
     let simauth = "Bearer " + data;
     console.log(simauth);
-    return axios.post(simurl,{
-
+    return fetch(simurl,{
+      method: 'POST',
       headers: {
-          "Authorization": simauth
+        "Authorization": simauth,
+        "Content-Type" : 'application/json'
       },
-      json: {
-        "ShortCode": "600383",
-        "CommandID": "CustomerPayBillOnline",
-        "Amount": "100",
-        "Msisdn": "254748905088",
-        "BillRefNumber": "TestAPI"
-      }
+      body: JSON.stringify({
+          ShortCode: "600383",
+          CommandID: "CustomerPayBillOnline",
+          Amount: "100",
+          Msisdn: "254748905088",
+          BillRefNumber: "TestAPI"
+      })
   }).then(function (response) {
       // handle success
       return response;
